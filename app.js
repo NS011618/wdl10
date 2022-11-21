4a6e5eb7f1209f36d0f0002f14cb5f92c00a639f
NS011618 <ashishgolla2003@gmail.com>
2022-11-21 20:22:22 +0530

    lvl10

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var cookieParser = require("cookie-parser");
 app.use(bodyParser.json());
 
 const path = require("path");
-const { Todo } = require("./models");
+const { Todo,User} = require("./models");
 // eslint-disable-next-line no-unused-vars
 const todo = require("./models/todo");
 //app.use()
@@ -15,16 +15,70 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser("shh! some secret string"));
 app.use(csrf("this_should_be_32_charactes_long", ["POST", "PUT", "DELETE"]));
 
+//passport
+const passport=require('passport');
+const connectEnsureLogin=require('connect-ensure-login');
+const session=require('express-session');
+const LocalStrategy=require('passport-local');
+
+app.use(session({
+  secret:"my-super-secret-key-2172817261561562",
+  cookie:{
+    maxAge:24*60*60*60*1000
+  }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy({
+  usernameField:'email',
+  passwordField:'password'
+},(username,password,done)=>{
+  User.findOne({where:{email:username,password:password}})
+  .then((user)=>{
+    return done(null,user)
+  }).catch((error)=>{
+    return (error);
+  })
+}));
+
+passport.serializeUser((user,done)=>{
+  console.log("serial user in session",user.id);
+  done(null,user.id);
+});
+
+passport.deserializeUser((id,done)=>{
+  User.findByPk(id)
+  .then(user=>{
+    done(null,user)
+  })
+  .catch(error=>{
+    done(error,null)
+  })
+});
+
+
 //app.get()
 app.set("view engine", "ejs");
 app.get("/", async (request, response) => {
+ 
+    response.render("index", {
+      title: "Todo Application",     
+      csrfToken: request.csrfToken(),
+    });
+  
+});
+
+app.get("/todos",connectEnsureLogin.ensureLoggedIn() ,async (request, response) => {
+  
   const allTodos = await Todo.getTodos();
   const overdue = await Todo.overdue();
   const dueLater = await Todo.dueLater();
   const dueToday = await Todo.dueToday();
   const completedItems = await Todo.completedItems();
   if (request.accepts("html")) {
-    response.render("index", {
+    response.render("todo", {
       title: "Todo Application",
       allTodos,
       overdue,
@@ -38,26 +92,50 @@ app.get("/", async (request, response) => {
   }
 });
 
-app.get("/todos", async (request, response) => {
-  // defining route to displaying message
-  console.log("Todo list");
-  try {
-    const todoslist = await Todo.findAll();
-    return response.json(todoslist);
-  } catch (error) {
-    console.log(error);
-    return response.status(422).json(error);
+// app.get("/todos", async (request, response) => {
+//   // defining route to displaying message
+//   console.log("Todo list");
+//   try {
+//     const todoslist = await Todo.findAll();
+//     return response.json(todoslist);
+//   } catch (error) {
+//     console.log(error);
+//     return response.status(422).json(error);
+//   }
+// });
+// app.get("/todos/:id", async function (request, response) {
+//   try {
+//     const todo = await Todo.findByPk(request.params.id);
+//     return response.json(todo);
+//   } catch (error) {
+//     console.log(error);
+//     return response.status(422).json(error);
+//   }
+// });
+//signup page
+app.get("/signup",async (request, response)=>{
+   response.render("signup",{csrfToken:request.csrfToken()})
+})
+//users page
+app.post("/users",async (request, response)=>{
+  try{
+    const user= await User.create({
+      firstName:request.body.firstName,
+      lastName:request.body.lastName,
+      email:request.body.email,
+      password:request.body.password
+    });
+    request.login(user,(err)=>{
+      if(err){
+        console.log(err)
+      }
+      response.redirect("/todos");
+    })    
+  }catch (error) {
+   console.log(error);
   }
-});
-app.get("/todos/:id", async function (request, response) {
-  try {
-    const todo = await Todo.findByPk(request.params.id);
-    return response.json(todo);
-  } catch (error) {
-    console.log(error);
-    return response.status(422).json(error);
-  }
-});
+})
+
 
 //app.post()
 app.post("/todos", async (request, response) => {
